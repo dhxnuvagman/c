59b0948906e9d97ee60f6cec4b64d53df610f329
Dhanush <130652450+dhxnuvagman@users.noreply.github.com>
2024-06-17 13:10:13 +0530

    Pattern printing

---
+++ 04.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+int i,j,rows;
+printf("Input number of rows : ");
+scanf("%d",&rows);
+for(i=1;i<=rows;i++)
+{
+	for(j=1;j<=rows-i;j++)
+		printf(" ");
+	for(j=1;j<=i;j++)
+		printf("%d",j);
+	for(j=i-1;j>=1;j--)
+		printf("%d",j);
+printf("\n");
+}
+}
